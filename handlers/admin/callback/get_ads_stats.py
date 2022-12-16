f747b73a1aad5c9f1aaa902464ef908b1e0aa009
rubix0307 <miroshnichenkoartem0307@gmail.com>
2022-12-16 14:28:31 +0100

    edit contest rule and add contest in stats

--- handlers/admin/callback/get_ads_stats.py
+++ handlers/admin/callback/get_ads_stats.py
@@ -29,10 +29,19 @@ async def get_ads_stats(call: types.CallbackQuery, callback_data: dict()):
             FROM users 
             WHERE date = '{date_today}' 
             GROUP BY came_from;''')
+
+    l_day = date - datetime.timedelta(days=1)
+    date_last_day = f'{l_day.year}-{l_day.month}-{l_day.day}'
+    data_ads_l_day = sql(f'''SELECT came_from as name , COUNT(*) as count 
+            FROM users 
+            WHERE date = '{date_last_day}' 
+            GROUP BY came_from;''')
+
     
     today_sum = sum([td['count'] for td in data_ads_today])
-    markup = InlineKeyboardMarkup(row_width=3)
-    markup.add(*[get_home_button(f'FROM: {len(data_ads)}'), get_home_button(f'ALL: {all_user_count}'), get_home_button(f'''TODAY: {today_sum}''')])
+    l_sum = sum([td['count'] for td in data_ads_l_day])
+    markup = InlineKeyboardMarkup(row_width=4)
+    markup.add(*[get_home_button(f'FROM: {len(data_ads) + 1}'), get_home_button(f'ALL: {all_user_count}'), get_home_button(f'TODAY: {today_sum}'), get_home_button(f'LAST DAY: {l_sum}')])
 
     for ad in data_ads[:50]:
         name_ad = ad['name']
@@ -48,8 +57,38 @@ async def get_ads_stats(call: types.CallbackQuery, callback_data: dict()):
         if len(data) == 2:
             data.append(get_home_button(f'⠀'))
 
+        for l_day in data_ads_l_day:
+            name_ad_l_day = l_day['name']
+            count_l_day = l_day['count']
+            if name_ad == name_ad_l_day and count_l_day:
+                data.append(get_home_button(f'+{count_l_day}'))
+                break
+        if len(data) == 3:
+            data.append(get_home_button(f'⠀'))
+
         markup.add(*data)
     
+
+
+    all_self_user = sql(f'''SELECT COUNT(*) as count 
+            FROM users
+            WHERE NOT (came_from REGEXP '[^0-9]')
+            ''')[0]['count']
+
+    data_ads_today = sql(f'''SELECT COUNT(*) as count 
+            FROM users 
+            WHERE date = '{date_today}' AND NOT (came_from REGEXP '[^0-9]')''')
+
+    data_ads_last_day = sql(f'''SELECT COUNT(*) as count 
+            FROM users 
+            WHERE date = '{date_last_day}' AND NOT (came_from REGEXP '[^0-9]')''')
+        
+    markup.add(*[get_home_button('contest'),
+        get_home_button(f'{all_self_user}'),
+        get_home_button(f'''+{data_ads_today[0]['count']}'''),
+        get_home_button(f'''+{data_ads_last_day[0]['count']}''')
+    ])
+
     message_data = {
         'text': f'Ads statistics',
         'reply_markup': markup,
--- handlers/admin/message/follow_group.py
+++ handlers/admin/message/follow_group.py
@@ -24,57 +24,34 @@ def get_stats(num, max_users, successfully, errors, start_time, is_continue=True
 
 @dp.message_handler(state='*', commands=['mails_ph'])
 async def mails_ph(message: types.Message):
-    start_time = time.time()
-    all_users = sql('SELECT * FROM `users` ORDER BY `users`.`role_id` DESC')
-    max_users = len(all_users)
-    successfully = []
-    errors = []
-    admin_message = await bot.send_message(chat_id=ADMIN_ID, text='/mails_ph\nСтарт')
+    if message.from_user.id == ADMIN_ID:
+        start_time = time.time()
+        all_users = sql('SELECT * FROM `users` ORDER BY `users`.`role_id` DESC')
+        max_users = len(all_users)
+        successfully = []
+        errors = []
+        admin_message = await bot.send_message(chat_id=ADMIN_ID, text='/mails_ph\nСтарт')
 
-    for num, user in enumerate(all_users):
-        if not num % 10:
-            
-            msg = get_stats(num, max_users, successfully, errors, start_time, is_continue=True) 
-            await bot.edit_message_text(msg, chat_id=ADMIN_ID, message_id=admin_message.message_id)
-
-        start_message = sql(f'''SELECT * FROM `start_messages` WHERE user_id = {user['user_id']}''')
-
-        try:
-            try:
-
-                if start_message:
-                    start_messages_id = start_message[0]['message_id']
-                    try:
-                        is_unpin = await bot.unpin_chat_message(user['user_id'], start_messages_id)
-                    except:
-                        pass
-
-                    try:
-                        await bot.delete_message(user['user_id'], start_messages_id)
-                    except:
-                        pass
-
-                photo = await bot.send_photo(
-                    chat_id=user['user_id'],
-                    photo='https://obertivanie.com/bot_images/default/sub_to_group.png', 
-                    reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton(text=f'👩‍👦‍👦⠀В группу⠀👨‍👩‍👧', url='https://t.me/+aIOTdrZd3504NGUy')),
-                    protect_content=True,
-                    )
-                await bot.pin_chat_message(chat_id=user['user_id'], message_id=photo.message_id)
+        for num, user in enumerate(all_users):
+            if not num % 10:
                 
-                is_insert = sql(f'''INSERT INTO `start_messages`(`user_id`, `message_id`) VALUES ({user['user_id']},{photo.message_id})''', commit=True)
-                if not is_insert:
-                    sql(f'''UPDATE `start_messages` SET `message_id` = '{photo.message_id}' WHERE `start_messages`.`user_id` = {user['user_id']};''', commit=True)
+                msg = get_stats(num, max_users, successfully, errors, start_time, is_continue=True) 
+                await bot.edit_message_text(msg, chat_id=ADMIN_ID, message_id=admin_message.message_id)
 
-                sql(f'''UPDATE `users` SET `is_active` = '1' WHERE `users`.`user_id` = {user['user_id']};''')
-                successfully.append(user['user_id'])
-            except Exception as e:
+            try:
                 try:
-                    is_update_active = sql(f'''UPDATE `users` SET `is_active` = '0' WHERE `users`.`user_id` = {user['user_id']};''', commit=True)
-                finally:
-                    errors.append(user['user_id'])
-        except Exception as err:
-            print(errors)
 
-    msg = get_stats(num, max_users, successfully, errors, start_time, is_continue=False)
-    await bot.edit_message_text(msg, chat_id=ADMIN_ID, message_id=admin_message.message_id)
+                    await bot.forward_message(user['user_id'], GROUG_ID, message_id=530)
+
+                    sql(f'''UPDATE `users` SET `is_active` = '1' WHERE `users`.`user_id` = {user['user_id']};''')
+                    successfully.append(user['user_id'])
+                except Exception as e:
+                    try:
+                        is_update_active = sql(f'''UPDATE `users` SET `is_active` = '0' WHERE `users`.`user_id` = {user['user_id']};''', commit=True)
+                    finally:
+                        errors.append(user['user_id'])
+            except Exception as err:
+                print(errors)
+
+        msg = get_stats(num, max_users, successfully, errors, start_time, is_continue=False)
+        await bot.edit_message_text(msg, chat_id=ADMIN_ID, message_id=admin_message.message_id)
